03ebcf82b39576ef448a6dec4c20ac252094e5a0
Baishant01 <baishantpachhai0@gmail.com>
2026-02-25 21:31:01 +0545

    Modified post details

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list' ),
-    path('post_details/', views.post_details, name='post_details' ),
+    path('post_details/<int:pk>/', views.post_details, name='post_details' ),
     path('post_form/', views.post_form, name='post_form' ),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 from .models import Post
 from django.contrib import messages
@@ -9,8 +9,13 @@ def post_list (request):
     posts = Post.objects.all().order_by('-created_at')
     return render (request, 'blog_app/post_list.html', {'posts':posts})
 
-def post_details (request):
-    return render (request, 'blog_app/post_detail.html')
+def post_details (request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if post:
+        return render (request, 'blog_app/post_detail.html', {'post':post})
+    else:
+        messages.error(request, 'Error while fetching details...')
+    return render (request, 'blog_app/post_list.html')
 
 def post_form (request):
     if request.method == "POST":
